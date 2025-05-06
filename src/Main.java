e66d18d69c91aa90b6fe4da1c4d7eee49157f44d
pobmic15 <pobmic15@gasos-ro.cz>
2025-05-06 08:12:04 +0200

    e

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import abstrakce.Had;
-import abstrakce.Pes;
-import abstrakce.Zelva;
+import abstrakce.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,9 +6,19 @@ public class Main {
         Pes pes = new Pes(2,"pepa");
         pes.vypisJmenoADatum();
         pes.vydejZvuk();
+        System.out.println("------------------------------------------------------------------");
         Zelva zelva = new Zelva(5,"honzicek");
         zelva.vypisJmenoADatum();
         zelva.vydejZvuk();
+        System.out.println("------------------------------------------------------------------");
+        VodniZelva vodniZelva = new VodniZelva(10,"ojojojbobritobonditou");
+        vodniZelva.vypisJmenoADatum();
+        vodniZelva.vydejZvuk();
+        System.out.println("------------------------------------------------------------------");
+        SuchozemskaZelva suchozemskaZelva = new SuchozemskaZelva(1,"lirililarila");
+        suchozemskaZelva.vypisJmenoADatum();
+        suchozemskaZelva.vydejZvuk();
+        System.out.println("------------------------------------------------------------------");
         Had had = new Had(3,"franta");
         had.vypisJmenoADatum();
         had.vydejZvuk();
